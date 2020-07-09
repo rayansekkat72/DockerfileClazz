9d6cb6071cddaad0d96a3f4f12e9f320b45f9e0c
rayansekkat72 <r.sekkat@unyc.io>
2020-07-09 15:43:15 +0200

    add !

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Hello to Your Vue.js App"/>
+    <HelloWorld msg="Hello to Your Vue.js App, RAYAN"/>
   </div>
 </template>
 
