6f0fab994d3b96c2a06b663e9229f6cabc483131
rayansekkat72 <r.sekkat@unyc.io>
2020-07-16 15:40:34 +0200

    Rayan = Con (add dot)

--- backend/backend/settings/local.py
+++ backend/backend/settings/local.py
@@ -1,10 +1,10 @@
 ALLOWED_HOSTS = [
     'backend.rayan.io',
     '127.0.0.1',
-    'backend.lperdereau.fr'
-    'frontend.lperdereau.fr'
-    '163.172.186.185'
-    '163.172.170.249'
-    '195.154.71.60'
+    'backend.lperdereau.fr',
+    'frontend.lperdereau.fr',
+    '163.172.186.185',
+    '163.172.170.249',
+    '195.154.71.60',
 ]
 globals().update(locals())
