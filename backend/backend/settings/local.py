19c20810b65eeee8f897d771cd60304c7a35b1b7
rayansekkat72 <r.sekkat@unyc.io>
2020-07-16 11:42:01 +0200

    add scaleway ip

--- backend/backend/settings/local.py
+++ backend/backend/settings/local.py
@@ -1,5 +1,9 @@
 ALLOWED_HOSTS = [
     'backend.rayan.io',
     '127.0.0.1',
+    'backend.lperdereau.fr'
+    'frontend.lperdereau.fr'
+    '163.172.186.185'
+    '163.172.170.249'
 ]
 globals().update(locals())
